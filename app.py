8a32aa9b25ed26f10cc2bfd98f29537ab1f69279
Temtemthanaporn06102003 <53107000+Temtemthanaporn06102003@users.noreply.github.com>
2019-10-02 08:42:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def callback():
     userText = decoded["queryResult"]['intent']['displayName']
     #sendText(user,userText)
     if (userText == 'สวัสดี') :
-        sendText(user,'เออออออออว่ะ')
+        sendText(user,'ว่างัยยยย')
     elif (userText == 'ไปล่ะ') :
         sendText(user,'ไปไหนก็ไป ไป๊')
     else :
